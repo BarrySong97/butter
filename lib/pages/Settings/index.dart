83502c6ee86c48330fd4062befba62f7146a9976
BaryySong97 <524000659@qq.com>
2022-12-26 12:38:27 +0800

    feat(settings): add url laungch of about me

--- lib/pages/Settings/index.dart
+++ lib/pages/Settings/index.dart
@@ -1,8 +1,11 @@
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'components/Lang.dart';
+
+final String aboutmeUrl = 'https://zhuanlan.zhihu.com/p/593575133?';
 
 class Settings extends StatelessWidget {
   @override
@@ -17,27 +20,37 @@ class Settings extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontSize: 16),
         ),
       ),
-      body: Column(
-        children: [buildLocale(context)],
+      body: Column(children: [buildLocale(context), buildAboutItem()]),
+    );
+  }
+
+  Widget buildAboutItem() {
+    return ListTile(
+      onTap: _launchUrl,
+      title: Text(
+        'aboutme'.tr,
+        style: TextStyle(color: Colors.white),
+      ),
+      subtitle: Text(
+        'aboutmeSub'.tr,
+        style: TextStyle(color: Colors.white, fontSize: 12),
       ),
     );
   }
 
-  // Widget buildColorItem(BuildContext context) {
-  //   return ListTile(
-  //     onTap: () => _selectColor(context),
-  //     title: Text('pickThemeColor'.tr),
-  //     subtitle: Text('pickThemeColorSub'.tr),
-  //     trailing: Container(
-  //       width: 24,
-  //       height: 24,
-  //       decoration: BoxDecoration(
-  //         borderRadius: BorderRadius.circular(8),
-  //         color: Theme.of(context).primaryColor,
-  //       ),
-  //     ),
-  //   );
-  // }
+  Widget buildAd() {
+    return ListTile(
+      // onTap: _launchUrl,
+      title: Text(
+        'dontTouch'.tr,
+        style: TextStyle(color: Colors.white),
+      ),
+      subtitle: Text(
+        'dontTouchSub'.tr,
+        style: TextStyle(color: Colors.white, fontSize: 12),
+      ),
+    );
+  }
 
   Widget buildLocale(BuildContext context) {
     return ListTile(
@@ -56,6 +69,12 @@ class Settings extends StatelessWidget {
         child: Icon(Icons.language, color: Colors.white),
       ),
     );
+  }
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(Uri.parse(aboutmeUrl))) {
+      throw 'Could not launch $aboutmeUrl';
+    }
   }
 
   void _selectLang(BuildContext context) {
